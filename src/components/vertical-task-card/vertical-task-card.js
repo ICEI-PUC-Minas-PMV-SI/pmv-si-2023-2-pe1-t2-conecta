5e1e18584734201c83a75220d68bcaaa4e88679b
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-11-02 19:46:56 -0300

    Corrige o caminha para carregamento de imagem de acordo com a página

--- src/components/vertical-task-card/vertical-task-card.js
+++ src/components/vertical-task-card/vertical-task-card.js
@@ -1,4 +1,5 @@
 const makeTemplate = () => {
+    const rootPath = window.location.pathname.includes('index.html') ? './' : '../../';
     const template = document.createElement('template');
     template.innerHTML = `
     <div class="root">
@@ -25,7 +26,7 @@ const makeTemplate = () => {
                         </div>
                     </div>
                     <div class="location-button-wrapper">
-                        <img class="image-location" src="../../assets/icons/location.png" alt="Location">
+                        <img class="image-location" src="${rootPath}/assets/icons/location.png" alt="Location">
                         <div class="location-tag"></div>
                     </div>
                 </div>
