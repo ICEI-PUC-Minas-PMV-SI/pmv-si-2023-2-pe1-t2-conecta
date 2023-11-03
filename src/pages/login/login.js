2715559baff9cee27333974e7a3797a9319ff425
Joao Zarate <joaozarate@gmail.com>
2023-11-03 16:07:59 +0000

    Implementação das telas.

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,5 +1,20 @@
 document.getElementById("entrar").addEventListener("click", handleGetIn);
 
+function handleForgotPassword() {
+    const email = document.getElementById("email").value;
+
+    const regex = /\S+@\S+\.\S+/;
+    if (!regex.test(email)) {
+        alert("Email inválido.");
+        return;
+    }
+
+    alert("Recuperação de senha realizado com sucesso. Verifique seu e-mail.");
+    window.location.href = "../../index.html";
+}
+
+document.getElementById("esqueceu-senha").addEventListener("click", handleForgotPassword);
+
 function handleGetIn(event) {
     event.preventDefault();
 
