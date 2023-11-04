5cfb776bc5e01ce64cb75bfe9e24bd49e63366d3
Joao Zarate <jlzteixeira@sga.pucminas.br>
2023-11-04 02:47:45 +0000

    Adiciona validações e ações. (#201)

---
+++ src/js/constants.js
@@ -0,0 +1,9 @@
+export let Required = (field) => {
+    return field + ' é obrigatório.';
+}
+
+export let SUCESSO_CADASTRO_DEMANDA = 'Demanda cadastrada com sucesso.';
+
+export let CONFIRM_CANCELAR_CADASTRO = 'Deseja cancelar o cadastro?';
+
+export let LOCATION_REF_ADMINISTRAR_DEMANDAS = '../administrar-demandas/administrar-demandas.html';
