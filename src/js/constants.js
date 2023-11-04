e82e5e1235fd52d7a952b7ff447bd9a041f9fbe1
Joao Zarate <joaozarate@gmail.com>
2023-11-04 20:07:46 +0000

    Adiciona validações e ações.

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,7 +3,9 @@ export let Required = (field) => {
 }
 
 export let SUCESSO_CADASTRO_DEMANDA = 'Demanda cadastrada com sucesso.';
+export let SUCESSO_ENVIAR_CANDIDATURA = 'Candidatura enviada com sucesso.';
 
 export let CONFIRM_CANCELAR_CADASTRO = 'Deseja cancelar o cadastro?';
 
 export let LOCATION_REF_ADMINISTRAR_DEMANDAS = '../administrar-demandas/administrar-demandas.html';
+export let LOCATION_REF_PAGINA_DEMANDAS = '../pagina-de-demandas/pagina-de-demandas.html';
