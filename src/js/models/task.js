0725492fe15705de98568a2305027b8a5ee53ab6
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-11-11 18:09:16 -0300

    Cria objeto de dominio: Task

---
+++ src/js/models/task.js
@@ -0,0 +1,54 @@
+import { makeRequest, getURL } from '../http.js';
+
+export class Task {
+    organizationId;
+    name;
+    description;
+    createdAt;
+    status;
+    type;
+
+    async create() {
+        const data = {
+            organizationId: this.organizationId,
+            name: this.name,
+            description: this.description,
+            createdAt: this.createdAt,
+            status: this.status,
+            type: this.type,
+            searchData: this.name + ' ' + this.description,
+        }
+
+        return await makeRequest(getURL('tasks'), 'POST', data);
+    }
+
+    async findById(id) {
+        return await makeRequest(getURL(`tasks/${id}`), 'GET');
+    }
+
+    async findByOrganizationId(organizationId) {
+        return await makeRequest(getURL(`tasks?organizationId=${organizationId}`), 'GET');
+    }
+
+    async findAll() {
+        return await makeRequest(getURL('tasks'), 'GET');
+    }
+
+    async updateById(id) {
+        const data = {
+            organizationId: this.organizationId,
+            name: this.name,
+            description: this.description,
+            createdAt: this.createdAt,
+            status: this.status,
+            type: this.type,
+            searchData: this.name + ' ' + this.description,
+        }
+
+        return await makeRequest(getURL(`tasks/${id}`), 'PUT', data);
+    }
+
+    async deleteById(id) {
+        return await makeRequest(getURL(`tasks/${id}`), 'DELETE');
+    }
+}
