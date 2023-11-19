12cf79f1d580fb56d9f6ca8d9923b865aefaf08f
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-11-19 11:01:09 -0300

    Permite utilizar o comando 'Control + v' para o campo CNPJ

--- src/pages/cadastrar-ong/cadastrar-ong-1.js
+++ src/pages/cadastrar-ong/cadastrar-ong-1.js
@@ -27,7 +27,9 @@ function isSpecialKey(event) {
         event.key === "Backspace" ||
         event.key === "Delete" ||
         event.key === "ArrowLeft" ||
-        event.key === "ArrowRight"
+        event.key === "ArrowRight" ||
+        (event.ctrlKey && event.key === "v") ||
+        (event.ctrlKey && event.key === "V")
     );
 }
 
