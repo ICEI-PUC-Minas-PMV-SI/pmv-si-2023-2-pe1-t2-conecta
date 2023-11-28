644d34d68beb1f46a089231e711ac748a94e3c33
AliceAbreu <alicereis1960@gmail.com>
2023-11-28 10:40:25 -0300

    Arruma modais em paginas de administrar demandas

--- src/js/landing-page.js
+++ src/js/landing-page.js
@@ -92,28 +92,3 @@ function showSlides_auto() {
   slides[slideIndex2-1].style.display = "block";
   setTimeout(showSlides_auto, 4000); // Change image every 2 seconds
 }
-
-//modal.js
-function modal(item, event) {
-    let title = item.find('.task-name').text()
-    $('.job-title').html(title)
-    
-    let text = item.find('.task-description-text').text()
-    $('.text').html(text)
-
-    let NGO = item.find('.task-owner').text()
-    $('.NGO').html(NGO)
-
-    let city = item.find('.location-tag').text()
-    $('.city').html(city)
-
-    $('#myModal').css('display', 'block');
-}
-
-$(".close").click(function(){
-    $('#myModal').css('display', 'none');
-});
-
-function fotoClick(event) {
-    event.stopPropagation();
-}
--- src/pages/pagina-de-demandas/pagina-de-demandas.js
+++ src/pages/pagina-de-demandas/pagina-de-demandas.js
@@ -14,32 +14,6 @@ $("#filtroSelect").click(function(){
     }
 });
 
-
-//modal.js
-function modal(item, event) {
-        let title = item.find('.task-name').text()
-        $('.job-title').html(title)
-        
-        let text = item.find('.task-description-text').text()
-        $('.text').html(text)
-
-        let NGO = item.find('.task-owner').text()
-        $('.NGO').html(NGO)
-
-        let city = item.find('.location-tag').text()
-        $('.city').html(city)
-
-        $('#myModal').css('display', 'block');
-}
-
-$(".close").click(function(){
-        $('#myModal').css('display', 'none');
-});
-
-function fotoClick(event) {
-        event.stopPropagation();
-}
-
 //filtro.js
 function buscaCidade(element) {
         $('.cidades_selecionadas').remove();
