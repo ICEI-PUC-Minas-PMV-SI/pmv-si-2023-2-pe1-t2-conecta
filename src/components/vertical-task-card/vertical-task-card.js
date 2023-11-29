aed200739b4592251704caee3db986ece2870391
AliceAbreu <alicereis1960@gmail.com>
2023-11-29 11:53:43 -0300

    Adiciona funcionalidades pagina de demandas

--- src/components/vertical-task-card/vertical-task-card.js
+++ src/components/vertical-task-card/vertical-task-card.js
@@ -48,6 +48,12 @@ const makeTemplate = () => {
                     </div>
                     
                 </div>
+
+                <div style="display:none">
+                    <div class="addres">
+                    </div>
+                </div>
+                
             </div>
         </div>
     </div>
@@ -287,11 +293,16 @@ export class VerticalTaskCard extends HTMLElement {
             helpButton.href = this.destination;
         }
 
+        if (this.addres) {
+            const addres = this.root.querySelector('.addres');
+            addres.innerText = this.addres;
+        }
+
     }
 
     static
     get observedAttributes() {
-        return ['name', 'owner', 'description', 'type', 'image', 'destination'];
+        return ['name', 'owner', 'description', 'type', 'image', 'destination', 'addres'];
     }
 
     get name() {
@@ -340,6 +351,14 @@ export class VerticalTaskCard extends HTMLElement {
 
     set destination(value) {
         this.setAttribute('destination', value);
+    }
+
+    get addres() {
+        return this.getAttribute('addres');
+    }
+
+    set addres(value) {
+        this.setAttribute('addres', value);
     }
 }
 
--- src/pages/pagina-de-demandas/pagina-de-demandas.js
+++ src/pages/pagina-de-demandas/pagina-de-demandas.js
@@ -1,3 +1,108 @@
+import { Organization } from "../../js/models/organization.js";
+import { Task } from "../../js/models/task.js";
+import { VerticalTaskCard } from "../../components/vertical-task-card/vertical-task-card.js";
+import { HorizontalTaskCard } from "../../components/horizontal-task-card/horizontal-task-card.js";
+
+const descriptions = document.querySelectorAll('.task-description > p');
+
+descriptions.forEach(description => {
+    const showMoreButton = description.nextElementSibling; // Assuming the button is a sibling element
+
+    // check if the description needs a 'Show More' button
+    if(description.offsetHeight < description.scrollHeight) {
+        showMoreButton.style.display = 'block';
+    }
+});
+
+const getOrganizationId = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('id');
+}
+
+const getOrganizationData = async (id) => {
+    const ong = new Organization();
+    return await ong.findById(id);
+}
+
+function addFeedbackItem(name, date, text) {
+    const feedbackItem = document.createElement('div');
+    feedbackItem.className = 'feedback-item';
+
+    const feedbackHeader = document.createElement('div');
+    feedbackHeader.className = 'feedback-header';
+
+    const feedbackName = document.createElement('p');
+    feedbackName.className = 'feedback-name';
+    feedbackName.innerText = name;
+
+    const feedbackDate = document.createElement('p');
+    feedbackDate.className = 'feedback-date';
+    const dateObj = new Date(date);
+    const dateToLocale = dateObj.toLocaleDateString('pt-BR', { month: 'long', year: 'numeric' });
+    const formattedDate = dateToLocale.replace(' de ', ', ');
+    feedbackDate.innerText = formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+
+    feedbackHeader.appendChild(feedbackName);
+    feedbackHeader.appendChild(feedbackDate);
+
+    const feedbackText = document.createElement('p');
+    feedbackText.className = 'feedback-text';
+    feedbackText.innerText = text;
+
+    feedbackItem.appendChild(feedbackHeader);
+    feedbackItem.appendChild(feedbackText);
+
+    const feedbackWrapper = document.querySelector('.feedback-wrapper');
+    feedbackWrapper.appendChild(feedbackItem);
+}
+
+const populateDemanda = async () => {
+    const tasksWrapper = document.getElementById('task-wrapper');
+    const task = new Task();
+    const tasks = await task.findAll();
+
+    tasks.forEach(async task => {
+        const verticalTaskCard = new VerticalTaskCard();
+        let organizationData = await getOrganizationData(task.organizationId);
+        verticalTaskCard.name = task.name;
+        verticalTaskCard.description = task.description;
+        if(task.type == 'Presencial') {
+                verticalTaskCard.type = organizationData.city+', '+organizationData.state;
+        } else {
+                verticalTaskCard.type = task.type.toUpperCase();
+        }
+        verticalTaskCard.destination = `../candidatar-a-demanda/candidatar-a-demanda.html?id=${task.id}`;
+        verticalTaskCard.owner = organizationData.name
+        verticalTaskCard.image = organizationData.image;
+        verticalTaskCard.addres = organizationData.street+', '+organizationData.number
+
+        tasksWrapper.appendChild(verticalTaskCard);
+    });
+
+    tasks.forEach(task => {
+        const horizontalTaskCard = new HorizontalTaskCard();
+
+        horizontalTaskCard.name = task.name;
+        horizontalTaskCard.description = task.description;
+        horizontalTaskCard.type = task.type.toUpperCase();
+        horizontalTaskCard.destination = `../candidatar-a-demanda/candidatar-a-demanda.html?id=${task.id}`;
+        horizontalTaskCard.owner = organizationData.name
+        horizontalTaskCard.image = organizationData.image;
+
+
+        tasksWrapper.appendChild(horizontalTaskCard);
+
+    });
+}
+
+populateDemanda().then().catch(err => console.log(err));
+
+
+
+
+
+//filtro.js
+
 $("#filtroSelect").click(function(){
     if ($('.filterButton').hasClass('visible')) {
         $('.filterButton').addClass('invisible')
